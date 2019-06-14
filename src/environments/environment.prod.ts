556ca0fade3194d707fdd0f852a414656b6917fb
Anthony Manalakos <anthony.manalakos@gmail.com>
2019-06-14 17:15:17 -0500

    adding prod environment to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4200'
+  firebase: {
+    apiKey: "AIzaSyCskPYtvjiQPxnTcVZYHoE4re6nsHcU0As",
+    authDomain: "webapps-ed273.firebaseapp.com",
+    databaseURL: "https://webapps-ed273.firebaseio.com",
+    projectId: "webapps-ed273",
+    storageBucket: "webapps-ed273.appspot.com",
+    messagingSenderId: "1030103240616",
+    appId: "1:1030103240616:web:1914fe587d201039"
+ },
+ apiUrl: 'http://localhost:4200'
 };
